9baa78c793b4c92bfe1ee7d862c9afbb171f3afe
Christopher Biggs <christopher@accelerando.com.au>
2024-05-15 15:29:30 +1000

    Accelerando hailstone and redsoil boards

---
+++ ports/nrf/boards/accelerando_redsoil/mpconfigboard.mk
@@ -0,0 +1,11 @@
+// USB ID 1209:ACC1 allocated by pidcodes.github.com
+USB_VID = 0x1209
+USB_PID = 0xACC1
+USB_PRODUCT = "Redsoil"
+USB_MANUFACTURER = "Accelerando"
+
+MCU_CHIP = nrf52840
+
+INTERNAL_FLASH_FILESYSTEM = 1
+
+MICROPY_PY_ASYNC_AWAIT = 0
